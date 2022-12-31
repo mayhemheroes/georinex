ea7a9d72f22d74c99255b4c9acaf9d7dbe5e5fcd
bcapuano <bcapuano@asu.edu>
2022-12-30 23:05:55 -0500

    simplify exception handler

--- mayhem/fuzz_load.py
+++ mayhem/fuzz_load.py
@@ -14,11 +14,7 @@ def TestOneInput(data):
         with fdp.ConsumeMemoryFile(all_data=True, as_bytes=False) as f:
             f.name = 'test.10o'
             gr.load(f)
-    except (AssertionError, KeyError) as e:
-        if random.random() > 0.99:
-            raise e
-        return -1
-    except IndexError as e:
+    except (AssertionError, KeyError, IndexError) as e:
         if random.random() > 0.99:
             raise e
         return -1
